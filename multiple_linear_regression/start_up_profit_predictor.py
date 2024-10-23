ab018aae03b506f90ec403a71f80d29e48a67680
Bhoopendra Kumar <bhoopendra.akgec@gmail.com>
2024-10-23 14:41:24 +0530

    Create start_up_profit_predictor.py

---
+++ multiple_linear_regression/start_up_profit_predictor.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+# Load the data
+dataset = pd.read_csv('50_Startups.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Encode the categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+print(x)
+# split the data into training and test set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+y_pred = regressor.predict(x_test)
+
+print('Actual:')
+print(y_test)
+print('Predicted:')
+print(y_pred)
+
+
