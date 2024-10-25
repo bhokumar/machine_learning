4ca05f35f2b6f2ae936cedef846e1d1813e41740
bhokumar <bhoopendra.akgec@gmail.com>
2024-10-25 11:07:47 +0530

    Refactoring code for model selection for classifiers

---
+++ classification/model_selection/decision_tree_classifier.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+# print(x_train)
+# print(x_test)
+# print(y_train)
+# print(y_test)
+
+
+# print(x_train)
+# print(x_test)
+
+from sklearn.tree import DecisionTreeClassifier
+
+classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+
+confusion_matrix = confusion_matrix(y_test, y_pred)
+
+print(confusion_matrix)
+print(accuracy_score(y_test, y_pred))
